7b2982c111e385736b0dc871543251195c4c023e
Suvadip Ghosh <suvadip91@yahoo.com>
2024-01-16 22:55:53 +0530

    initial commit

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-3xl">
+        <h1>This is react</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
