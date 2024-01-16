ac244decfbae965e17a818964a086b97976f3e1a
Suvadip Ghosh <suvadip91@yahoo.com>
2024-01-16 23:46:11 +0530

    implement button

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("red")
 
   return (
     <>
-      <div className="text-3xl scre">
-        <h1>This is react</h1>
+      <div className="w-full h-screen" style={{backgroundColor:color}}>
+        <div className='fixed flex flex-wrap justify-center
+         bg-gray-400 bottom-12 inset-x-0 px-2'>
+          <dev className="flex flex-wrap justify-center gap-3 shadow-lg bg-white
+          px-3 py-2 rounded-xl">
+            <button className='outline-none px-4 py-1 rounded-full
+             text-white shadow-lg' style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
+             <button className='outline-none px-4 py-1 rounded-full
+             text-white shadow-lg' style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>Blue</button>
+             <button className='outline-none px-4 py-1 rounded-full
+             text-black shadow-lg' style={{backgroundColor:"white"}} onClick={()=>setColor("white")}>White</button>
+             <button className='outline-none px-4 py-1 rounded-full
+             text-white shadow-lg' style={{backgroundColor:"black"}} onClick={()=>setColor("black")}>Black</button>
+          </dev>
+         </div>
       </div>
     </>
   )
